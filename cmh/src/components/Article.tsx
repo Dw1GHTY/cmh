bca8984e51b2d4a092f7b8c1c10b0dfe85b9f3b7
Marko <mih.mare@gmail.com>
2024-12-30 16:14:02 +0100

    Logo

--- cmh/src/components/Article.tsx
+++ cmh/src/components/Article.tsx
@@ -4,33 +4,18 @@ import { Separator } from "./ui/separator";
 interface ArticleProps {
   title: string;
   paragraps: string[];
-  component?: React.ReactNode;
 }
 
-//TODO: mark links in the paragraphs array of string with bits, create a link tag
-//TODO: in the text that is marked as such to the path which is in caps: LINK
-
-//TODO: same way, make a FLAG to mark the position the ReactNode should go
-//!Example: <Article paragraphs={["This is text and <this is a link> CON text texfsS CON "]}
-
-const Article: React.FC<ArticleProps> = (props) => {
-  const { title, paragraps } = props;
+const Article: React.FC<ArticleProps> = ({ title, paragraps }) => {
   return (
     <article className="flex flex-col justify-start bg-white w-full h-fit my-3 p-4 rounded-md shadow-md">
       <h3 className="text-green-600 text-xl">{title}</h3>
-      <Separator className="h-0.5 w-1/2 bg-green-400 rounded-md" />
-      <br />
-      {paragraps.map((parag, index) => {
-        //? 0-based index
-        return (
-          <>
-            <p key={index} className="text-black">
-              {parag}
-            </p>
-            <br />
-          </>
-        );
-      })}
+      <Separator className="h-0.5 w-1/2 bg-green-400 rounded-md my-2" />
+      {paragraps.map((paragraph, index) => (
+        <p key={index} className="text-gray-700 mb-4">
+          {paragraph}
+        </p>
+      ))}
     </article>
   );
 };
--- cmh/src/components/Logo.tsx
+++ cmh/src/components/Logo.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import React from "react";
 
@@ -7,13 +8,19 @@ const Logo: React.FC = () => {
   const router = useRouter();
 
   return (
-    <div
-      onClick={() => {
-        router.push("/");
-      }}
-      className="h-16 w-32 border-black border-2 rounded-md text-xl hover:cursor-pointer"
-    >
-      LOGO
+    <div className="flex items-center h-full rounded-md hover:shadow-lg hover:shadow-green-900">
+      <Image
+        className="cursor-pointer rounded-md"
+        onClick={() => {
+          router.push("/");
+        }}
+        alt="Complete Mobile Health logo"
+        src={`/cmh_logo_2.jpg`}
+        width={150}
+        height={40}
+        style={{ objectFit: "contain" }}
+        sizes="(max-width: 768px) 100px, (max-width: 1024px) 150px, 200px"
+      />
     </div>
   );
 };
