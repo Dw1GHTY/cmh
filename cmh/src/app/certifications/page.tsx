8418307fbf2cb9825064d7012767d4bd98fff233
Dw1GHTY <mih.mare@gmail.com>
2025-02-19 20:39:37 +0100

    content quickfix

--- cmh/src/app/certifications/page.tsx
+++ cmh/src/app/certifications/page.tsx
@@ -24,7 +24,7 @@ const page = () => {
             />
           }
           paragraps={[
-            "Midland Health is a Department of Transportation certified Disadvantaged Business Enterprise. Partnering with Midland may fulfill government-driven obligations. Learn about what it means to be DBE Certified.",
+            "Complete Mobile Health is a Department of Transportation certified Disadvantaged Business Enterprise. Partnering with Complete Mobile may fulfill government-driven obligations. Learn about what it means to be DBE Certified.",
             "To be certified as a DBE, a firm must be a small business owned and controlled by socially and economically disadvantaged individuals. Certifiers make the determination based upon on site visits, personal interviews, reviews of licenses, stock ownership, bonding capacity, work completed, resume of principal owners, and financial capacity.",
           ]}
         />
@@ -40,7 +40,7 @@ const page = () => {
             />
           }
           paragraps={[
-            "As a Women Owned Business, Midland Health is WBENC Certified by the LINKWomen’s Business Enterprise National CouncilLINKHREFhttps://www.wbenc.org/certification/.",
+            "As a Women Owned Business, Complete Mobile Health is WBENC Certified by the LINKWomen’s Business Enterprise National CouncilLINKHREFhttps://www.wbenc.org/certification/.",
             "Certification is one of WBENC’s three core values: Certification, Opportunities, and Resources. Certifying women owned businesses is the foundation of WBENC’s mission, along with connecting WBENC-Certified Women’s Business Enterprises (WBEs) with WBENC’s Corporate Members to facilitate real time business opportunities and serving as a Resource to offer training that helps the Corporate Member and the WBENC-Certified WBE grow their capacity. WBENC Certification validates that the business is 51 percent owned, controlled, operated, and managed by a woman or women.",
             "To achieve WBENC Certification, women owned businesses complete a formal documentation and site visit process which is administered by one of WBENC’s 14 Regional Partner Organizations. WBENC Certification gives women owned businesses the ability to compete for real-time business opportunities provided by WBENC Corporate Members and government agencies.",
           ]}
