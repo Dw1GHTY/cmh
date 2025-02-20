086ad2d4c188802cf3adee9f007a73afa2a031e8
Dw1GHTY <mih.mare@gmail.com>
2025-02-20 17:27:32 +0100

    content fix

--- cmh/src/app/certifications/page.tsx
+++ cmh/src/app/certifications/page.tsx
@@ -7,7 +7,7 @@ const page = () => {
   return (
     <div className="flex flex-col justify-center items-center w-full h-full">
       <PageImage
-        title="Pofessional Cerifications"
+        title="Pofessional Certifications"
         subtitle=""
         path="/certifications.jpg"
       />
@@ -28,7 +28,7 @@ const page = () => {
             "To be certified as a DBE, a firm must be a small business owned and controlled by socially and economically disadvantaged individuals. Certifiers make the determination based upon on site visits, personal interviews, reviews of licenses, stock ownership, bonding capacity, work completed, resume of principal owners, and financial capacity.",
           ]}
         />
-        <Article
+        {/* <Article
           title="Women’s Business Enterprise National Council (WBENC)"
           component={
             <Image
@@ -44,7 +44,7 @@ const page = () => {
             "Certification is one of WBENC’s three core values: Certification, Opportunities, and Resources. Certifying women owned businesses is the foundation of WBENC’s mission, along with connecting WBENC-Certified Women’s Business Enterprises (WBEs) with WBENC’s Corporate Members to facilitate real time business opportunities and serving as a Resource to offer training that helps the Corporate Member and the WBENC-Certified WBE grow their capacity. WBENC Certification validates that the business is 51 percent owned, controlled, operated, and managed by a woman or women.",
             "To achieve WBENC Certification, women owned businesses complete a formal documentation and site visit process which is administered by one of WBENC’s 14 Regional Partner Organizations. WBENC Certification gives women owned businesses the ability to compete for real-time business opportunities provided by WBENC Corporate Members and government agencies.",
           ]}
-        />
+        /> */}
       </section>
     </div>
   );
