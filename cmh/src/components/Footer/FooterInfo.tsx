9d3e5fd3630771166bfe73fe2149d8724f5ebd99
Marko <mih.mare@gmail.com>
2025-03-01 09:22:21 +0100

    address update

--- cmh/src/components/Footer/FooterInfo.tsx
+++ cmh/src/components/Footer/FooterInfo.tsx
@@ -3,8 +3,8 @@ import React from "react";
 const FooterInfo: React.FC = () => {
   return (
     <div className="flex flex-col">
-      <span>11803 W. North Ave, Suite 200</span>
-      <span>Milwaukee, Wisconsin 53226</span>
+      <span>4805 S 74th st,</span>
+      <span>Greenfield Wi 53220</span>
       <span>Phone: 414-213-6543</span>
     </div>
   );
